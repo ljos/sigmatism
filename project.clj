302ba647f5b1a2e43714f7d50bb4aadaa2a954eb
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-08-31 11:06:12 +0200

    not using core, but sigmatism as main file

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject sigmatism "0.0.1"
   :description "John McCarty's lisp implemented in different languages."
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :main sigmatism.clj.core)
+  :main sigmatism.clj.sigmatism)
