ad19b0131a0ba84d92b7811a4594fe3f9ba1f467
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-08-31 11:20:28 +0200

    update clojure version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject sigmatism "0.0.1"
   :description "John McCarty's lisp implemented in different languages."
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :main sigmatism.clj.sigmatism)
