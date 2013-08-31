ac2f4fa0314b9a377edd77d10dce5a0b55967fe0
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-08-31 11:07:49 +0200

    inline functions and refactor

--- src/sigmatism/clj/sigmatism.clj
+++ src/sigmatism/clj/sigmatism.clj
@@ -1,83 +1,46 @@
 (ns sigmatism.clj.sigmatism
-  (:refer-clojure :exclude [eval atom assoc not]))
+  (:refer-clojure :exclude [eval atom]))
 
 (defn eq [x y]
-  (cond (and (= x '()) (= y '()))
-        't
-        (and (symbol? x) (symbol? y) (= x y))
-        't
-        :else
-        '()))
-
-(defn assoc [x y]
-  (if (= 't (eq (ffirst y) x))
-    (-> y first fnext)
-    (assoc x (rest y))))
-
-(declare eval)
-
-(defn evcon [c a]
-  (if (= 't (eval (ffirst c) a))
-    (eval (-> c first fnext) a)
-    (evcon (rest c) a)))
+  (if (and (symbol? x) (symbol? y) (= x y)) 't 'nil))
 
 (defn null [x]
-  (eq x '()))
-
-(defn append [x y]
-  (if (= 't (null x))
-    y
-    (cons (first x) (append (rest x) y))))
-
-(defn not [x]
-  (if (= 't x)
-    '()
-    't))
+  (eq x 'nil))
 
 (defn atom [x]
-  (if (or (symbol? x)
-          (= '() x))
-    't
-    '()))
+  (if (symbol? x) 't 'nil))
 
-(defn pair [x y]
-  (cond (and (= 't (null x)) (= 't (null y)))
-        '()
+(defn eval
+  ([e]
+     (eval e {}))
+  ([e a]
+     (if (= 't (atom e))
+       (a e)
+       (let [[fun & args] e]
+         (cond (= 't (atom (first e)))
+               (cond
+                (= fun 'quote) (first args)
+                (= fun 'atom)  (atom (eval (first args) a))
+                (= fun 'eq)    (eq (eval (first args) a)
+                                   (eval (second args) a))
+                (= fun 'car)   (first (eval (first args) a))
+                (= fun 'cdr)   (rest (eval (first args) a))
+                (= fun 'cons)  (cons (eval (first args) a)
+                                     (eval (second args) a))
+                (= fun 'cond)  (loop [[[c ex] & conds] args]
+                                 (if-not (nil? (eval c a))
+                                   (eval ex a)
+                                   (recur conds)))
+                :else
+                (eval (cons (a fun) args) a))
 
-        (and (= 't (not (atom x))) (= 't (not (atom y))))
-        (cons (list (first x) (first y))
-              (pair (rest x) (rest y)))))
+               (= (ffirst e) 'label)
+               (let [[name body] (rest fun)]
+                 (eval (cons body args) (assoc a name fun)))
 
-(defn evlis [m a]
-  (if (= 't (null m))
-    '()
-    (cons (eval (first m) a)
-          (evlis (rest m) a))))
-
-(defn eval [e a]
-  (let [[fun & args] e]
-    (cond (= 't (atom e))
-          (assoc e a)
-
-          (= 't (atom fun))
-          (cond
-           (= fun 'quote) (first args)
-           (= fun 'atom)  (atom (eval (first args) a))
-           (= fun 'eq)    (eq (eval (first args) a)
-                              (eval (second args) a))
-           (= fun 'car)   (first (eval (first args) a))
-           (= fun 'cdr)   (rest (eval (first args) a))
-           (= fun 'cons)  (cons (eval (first args) a)
-                                (eval (second args) a))
-           (= fun 'cond)  (evcon args a)
-           :else
-           (eval (cons (assoc fun a) args) a))
-
-          (= (first fun) 'label)
-          (let [[name body] (rest fun)]
-            (eval (cons body args)
-                  (cons (list name fun) a)))
-
-          (= (first fun) 'lambda)
-          (let [[largs body] fun]
-            (eval body (append (pair largs (evlis args a)) a))))))
+               (= (ffirst e) 'lambda)
+               (let [[largs body] (rest fun)]
+                 (->> (map #(eval % a) args)
+                      (interleave largs)
+                      (apply (partial assoc a))
+                      (eval body))))))))
